482ae867bd23beded24c422511cd1c4065a1117c
ce7elem <olivier.benaben@protonmail.com>
2023-09-24 22:49:30 +0200

    fix: avoid duplicates in staged zone

--- vault-cli/src/cmd/remove.rs
+++ vault-cli/src/cmd/remove.rs
@@ -5,10 +5,9 @@ use std::io::Write;
 use std::path::Path;
 use crate::vault::get_staged_files;
 
-/// Add file or directory content to the current Vault collection
+/// Add file to the current Vault collection
 pub fn add(path: String) {
-    info!("Staging {path}");
-
+    info!("Unstaging {path}");
     let files: Vec<String> = if Path::new(&path).is_dir() {
         fs::read_dir(path)
             .unwrap()
@@ -23,24 +22,15 @@ pub fn add(path: String) {
     } else if Path::new(&path).is_file() {
         vec![fs::canonicalize(path).unwrap().display().to_string()]
     } else {
-        eprintln!("{path} is neither a file nor a directory. Aborting.");
-        return;
+        exit(0);
     };
 
-    // remove already staged files from the selection
-    let staged = get_staged_files();
-    let files_to_add: Vec<String> = files.clone().into_iter().filter(|f| !staged.contains(f)).collect();
-    if files_to_add.is_empty() {
-        println!("File(s) already staged. Nothing to do");
-        return;
-    }
-
-    // add files to the staging zone
-    let mut staging_conf_file = OpenOptions::new()
+    let mut file = OpenOptions::new()
         .append(true)
         .open(Config::staging_file())
         .unwrap();
-    if let Err(e) = writeln!(staging_conf_file, "{}", files_to_add.join("\n")) {
-        eprintln!("Couldn't write to staging file: {}", e);
+
+    if let Err(e) = writeln!(file, "{}", files.join("\n")) {
+        eprintln!("Couldn't write to file: {}", e);
     }
 }
